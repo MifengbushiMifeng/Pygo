c96768e77ceea829bbe22415c445d3e2bec6ef24
Jonathan Zhou <zhouronghui30@163.com>
2017-04-14 22:35:22 +0800

    do practice

--- practice/firstPY.py
+++ practice/firstPY.py
@@ -2,5 +2,22 @@
 print 'hello, world'
 print 'Will learn python hard and quickly'
 print '100 + 200 = ', 100 + 200
-name = raw_input('please enter your name:')
-print 'hello,', name
+# name = raw_input('please enter your name:')
+# print 'hello,', name
+
+# the logic
+a = 100
+if a >= 0:
+    print a
+else:
+    print -a
+
+# Shift
+print 'I\'m \"OK\"!'
+
+print 10 / 3
+print 10.0 / 3
+
+# ASCII
+print ord('A')
+print chr(65)
