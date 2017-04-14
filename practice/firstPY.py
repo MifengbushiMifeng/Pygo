aba8a9ff14b10c4fe5f8a3e654e34ec774a1c7a5
Jonathan Zhou <zhouronghui30@163.com>
2017-04-14 22:17:25 +0800

    practice input and output

--- practice/firstPY.py
+++ practice/firstPY.py
@@ -1,3 +1,6 @@
 # First python program
 print 'hello, world'
 print 'Will learn python hard and quickly'
+print '100 + 200 = ', 100 + 200
+name = raw_input('please enter your name:')
+print 'hello,', name
