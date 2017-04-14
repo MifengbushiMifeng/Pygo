2d3237acbc45317933278504bdd73e896aa287b7
Jonathan Zhou <zhouronghui30@163.com>
2017-04-15 00:03:18 +0800

    practice if logic in python

--- practice3/firstPY3.py
+++ practice3/firstPY3.py
@@ -12,3 +12,14 @@ changedTuple[2][0] = 'X'
 changedTuple[2][0] = 'Y'
 
 print changedTuple
+
+# Logic
+age = 3
+if age >= 18:
+    print 'your age is', age
+    print 'adult'
+elif age >= 6:
+    print 'your age is', age
+    print 'teenager'
+else:
+    print 'kid'
