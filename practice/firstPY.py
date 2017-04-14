fde6e3cb72abd2df3db03809dfd33778e627123a
Jonathan Zhou <zhouronghui30@163.com>
2017-04-14 22:56:37 +0800

    Practice for format string

--- practice/firstPY.py
+++ practice/firstPY.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # First python program
 print 'hello, world'
 print 'Will learn python hard and quickly'
@@ -21,3 +22,15 @@ print 10.0 / 3
 # ASCII
 print ord('A')
 print chr(65)
+
+print u'\u4e2d'
+print u'中'.encode('utf-8')
+print len('ABC')
+
+testStr = 'Hello, %s'
+testStr %= 'world'
+print testStr
+
+testStr2 = 'Hi, %s, you have $%d.'
+testStr2 %= ('Jonathan', 1000000)
+print testStr2
