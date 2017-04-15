d094d35a37ed700962ea7dd9549a8287c47a6aef
Jonathan Zhou <zhouronghui30@163.com>
2017-04-15 21:49:25 +0800

    define function in python

--- practice3/firstPY5.py
+++ practice3/firstPY5.py
@@ -9,3 +9,28 @@ print int('123'), int(12.34), float('12.34'), str(1.23), unicode(100), bool(2), 
 
 funcA = abs
 print funcA(-3)
+
+
+# define my first function in python
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+print my_abs(-103)
+print my_abs('A')
+
+
+def my_abs2(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+print my_abs2(-333)
+print my_abs2('A')
