8b68bae40a74c1a4edf2572760a05119dc48acaf
Jonathan Zhou <zhouronghui30@163.com>
2017-04-15 21:38:31 +0800

    take the first look of function in python

--- practice3/firstPY5.py
+++ practice3/firstPY5.py
@@ -1,3 +1,11 @@
 # coding=utf-8
 
+# practice function in python
+print abs(100), abs(-20)
 
+print cmp(1, 2), cmp(2, 1), cmp(3, 3)
+
+print int('123'), int(12.34), float('12.34'), str(1.23), unicode(100), bool(2), bool('')
+
+funcA = abs
+print funcA(-3)
