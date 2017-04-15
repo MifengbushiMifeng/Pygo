317e50add298d9370c7250e52acc5a0db7cce1cf
Jonathan Zhou <zhouronghui30@163.com>
2017-04-16 00:29:29 +0800

    continue practicing function in python

---
+++ practice3/firstPY8.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+def func(a, b, c=0, *args, **other):
+    print 'a=', a, 'b=', b, 'c=', c, 'args=', args, 'other=', other
+
+
+func(1, 2)
+func(1, 2, c=3)
+func(1, 2, 3, 'a', 'b')
+func(1, 2, 3, 'a', 'b', x=99)
+
+args = (1, 2, 3, 4)
+other = {'x': 99}
+func(*args, **other)
