2799960a602640d2b4b5b28496916ee1092190ec
Jonathan <zhouronghui30@163.com>
2017-04-17 15:12:04 +0800

    practice Filter in Python

--- practice3/firstPY13.py
+++ practice3/firstPY13.py
@@ -23,3 +23,15 @@ def not_empty(s):
 print not_empty('  ')
 
 print filter(not_empty, ['A', '', 'B', None, 'C', '  '])
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    for n in range(2, num):
+        if num % n == 0:
+            return False
+    return True
+
+
+print filter(is_prime, [1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 23])
