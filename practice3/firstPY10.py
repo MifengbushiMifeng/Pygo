99d37c36f317cb9e4da7f488e7f9afd309a05e60
Jonathan <zhouronghui30@163.com>
2017-04-17 10:34:15 +0800

    practice List Comprehensions in python

--- practice3/firstPY10.py
+++ practice3/firstPY10.py
@@ -14,5 +14,18 @@ print L3
 L4 = [m + n for m in 'ABCD' for n in 'ABCD']
 print L4
 
-list = [d for d in os.listdir('D:\Cisco')]
-print list
+# list = [d for d in os.listdir('D:\Cisco')]
+# print list
+
+d = {'x': 'A', 'y': 'B', 'z': 'C'}
+L5 = [k + '=' + v for k, v in d.iteritems()]
+print L5
+
+L6 = ['Hello', 'World', 'Apple', 'Cisco']
+print [s.lower() for s in L6]
+print [s.upper() for s in L6]
+
+x = 'abc'
+y = 123
+print isinstance(x, str)
+print isinstance(y, str)
