8488d4a73cd02efded94a96209cc971c57b87fba
Jonathan Zhou <zhouronghui30@163.com>
2017-04-24 22:36:34 +0800

    refactor  by add 'practice' folder as the root for practices

--- practice5/first_PY_7.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-from __future__ import division
-from PIL import Image
-
-im = Image.open('C:\Users\IBM_ADMIN\Downloads\BingWallpaper-2017-03-04.jpg')
-print im.format, im.size, im.mode
-
-print 10 / 3
--- practice8/hello.py
+++
@@ -1,6 +0,0 @@
-# !/usr/bin/env python
-# encoding=utf-8
-
-def application(environ, start_reapsonse):
-    start_reapsonse('200 OK', [('Content-Type', 'text/html')])
-    return '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
