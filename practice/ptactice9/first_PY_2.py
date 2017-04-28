56a9945ea527102e729d98d38a5dc463cbee1240
Jonathan <zhouronghui30@163.com>
2017-04-28 10:44:20 +0800

    practice '_' and '__' in python

--- practice/ptactice9/first_PY_2.py
+++ practice/ptactice9/first_PY_2.py
@@ -11,3 +11,13 @@ if __name__ == '__main__':
     foo(a=1, b=2, c=3)
     foo(1, 2, 3, 4, 5, a=1, b=2, c=3)
     foo('a', 1, None, a=1, b='2', c=3)
+
+
+class my_class(object):
+    def __init__(self, x, y):
+        self._x = x
+        self.__y = y
+
+
+obj = my_class('x', 'y')
+print obj._x, obj.__y
