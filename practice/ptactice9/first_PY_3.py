5bef9b1c96bba45f9fe4d5e9c487b7fd938eb146
Jonathan Zhou <zhouronghui30@163.com>
2017-05-01 23:42:41 +0800

    testing fot tuple

--- practice/ptactice9/first_PY_3.py
+++ practice/ptactice9/first_PY_3.py
@@ -9,3 +9,12 @@ print zip(x, y)
 t1 = ('a', 'b', 'b')
 t2 = (1, 2, 3)
 print zip(t1, t2)
+
+s = '121fds fecx gfdsfd'
+print ''.join(s)
+
+name = 'Jonathan'
+value = '27'
+unknow = (name, value)
+print unknow
+print type(unknow)
