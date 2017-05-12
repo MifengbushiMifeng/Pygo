0721d5ed9c57bf408a813e8a38df9126cdbee5f6
Jonathan <zhouronghui30@163.com>
2017-05-12 09:54:00 +0800

    practice 'file list' in python

--- practice/ptactice9/first_PY_7.py
+++ practice/ptactice9/first_PY_7.py
@@ -1,6 +1,18 @@
 # !/usr/bin/env python
 # encoding=utf-8
+import os
 
 L = ['Jonathan', 'Tim', 'Mike']
 j, t, m = L
 print j, t, m
+
+
+def file_list(file_path):
+    for root, dirs, files in os.walk(file_path):
+        print root
+        print dirs
+        for f in files:
+            print f
+
+
+file_list(r'C:\3All\S&M\Incidents\05.12 - bibit')
