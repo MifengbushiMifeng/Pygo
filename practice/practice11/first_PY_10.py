f26e1c7744b6b3c2455f0d6aa9ed6fd53924d4fb
Jonathan Zhou <zhouronghui30@163.com>
2017-05-15 00:18:38 +0800

    practice pythonic

--- practice/practice11/first_PY_10.py
+++ practice/practice11/first_PY_10.py
@@ -1,5 +1,7 @@
 # !/usr/bin/env python
 # encoding=utf-8
+from collections import defaultdict
+
 d = {'name': 'foo'}
 
 if d.has_key('name'):
@@ -9,3 +11,38 @@ else:
 
 print d.get('name')
 print d.get('age', 'un-know')
+
+print '---------------------'
+
+data = [('foo', 10), ('bar', 20), ('foo', 39), ('bar', 49)]
+
+groups = {}
+for (key, value) in data:
+    if key in groups:
+        groups[key].append(value)
+    else:
+        groups[key] = [value]
+print groups
+
+print '---------------------'
+
+groups = {}
+for (key, value) in data:
+    groups.setdefault(key, []).append(value)
+print groups
+
+groups = defaultdict(list)
+for (key, value) in data:
+    groups[key].append(value)
+print groups
+
+numbers = [1, 2, 3]
+my_dict = dict([(number, number * 2) for number in numbers])
+print my_dict
+
+# do it pythonic
+my_dict = {number: number * 2 for number in numbers}
+print my_dict
+
+my_dict = {number: number * 2 for number in numbers if number != 2}
+print my_dict
